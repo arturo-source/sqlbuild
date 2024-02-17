e67a2d38e5d7fadc313e2fa2b6cb1b266f341528
arturo-source <arturojuan9898@gmail.com>
2024-02-17 13:23:04 +0100

    Add go docs

--- common.go
+++ common.go
@@ -14,6 +14,7 @@ var (
 	ErrNoId     = errors.New("need an id in the structure")
 )
 
+// getStructName returns the name of the struct as a string
 func getStructName(s any) (sName string, err error) {
 	val := reflect.ValueOf(s)
 	kind := val.Kind()
@@ -29,6 +30,14 @@ func getStructName(s any) (sName string, err error) {
 	return
 }
 
+// getStructFields expects any struct and saves each field into a map[string]reflect.Value, it will set the Tag `db:""` as key of the map if it is set
+//
+// Example:
+//
+//	type Person struct {
+//		Name string `db:"name"`
+//		Age  int
+//	}
 func getStructFields(s any) Fields {
 	t := reflect.TypeOf(s)
 	v := reflect.ValueOf(s)
@@ -47,6 +56,7 @@ func getStructFields(s any) Fields {
 	return fields
 }
 
+// getIdFromFields finds id case insensitive inside the fields. Returns the original id key, and its value
 func getIdFromFields(fields Fields) (key string, value reflect.Value, err error) {
 	for fieldName, fieldValue := range fields {
 		if strings.ToLower(fieldName) == "id" {
@@ -60,6 +70,7 @@ func getIdFromFields(fields Fields) (key string, value reflect.Value, err error)
 	return
 }
 
+// sanitizeInput format like fmt.Sprintf, but sanitizes args to avoid sql injections
 func sanitizeInput(query string, args ...any) string {
 	for i := range args {
 		if argStr, ok := args[i].(string); ok {
