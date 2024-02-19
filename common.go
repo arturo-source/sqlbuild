d3c0701359dce1e01514ed48e239025f3b236852
arturo-source <arturojuan9898@gmail.com>
2024-02-19 10:48:28 +0100

    Reorder the common funcs

--- common.go
+++ common.go
@@ -7,52 +7,14 @@ import (
 	"strings"
 )
 
-type Fields struct {
-	namesOrdered []string
-	nameValues   map[string]reflect.Value
-}
-
-func (f *Fields) len() int {
-	return len(f.namesOrdered)
-}
-
-func (f *Fields) set(k string, v reflect.Value) {
-	f.namesOrdered = append(f.namesOrdered, k)
-	f.nameValues[k] = v
-}
-
-func (f *Fields) get(k string) reflect.Value {
-	return f.nameValues[k]
-}
-
-func (f *Fields) getNames() []string {
-	return f.namesOrdered
-}
-
 var (
 	ErrNoStruct = errors.New("provided value is not a struct")
 	ErrNoId     = errors.New("need an id in the structure")
 )
 
-// getStructFromPointer unreferences the pointer until it gets a struct
-func getStructFromPointer(s any) (val reflect.Value, err error) {
-	val = reflect.ValueOf(s)
-
-	kind := val.Kind()
-	if kind == reflect.Pointer {
-		return getStructFromPointer(val.Elem().Interface())
-	}
-
-	if kind != reflect.Struct {
-		err = ErrNoStruct
-	}
-
-	return
-}
-
-// getStructName returns the name of the struct as a string
-func getStructName(val reflect.Value) string {
-	return reflect.TypeOf(val.Interface()).Name()
+type Fields struct {
+	namesOrdered []string
+	nameValues   map[string]reflect.Value
 }
 
 // newFields (from a struct) saves each field into a map[string]reflect.Value, it will set the Tag `db:""` as key of the map if it is set
@@ -80,6 +42,23 @@ func newFields(val reflect.Value) Fields {
 	return fields
 }
 
+func (f *Fields) len() int {
+	return len(f.namesOrdered)
+}
+
+func (f *Fields) set(k string, v reflect.Value) {
+	f.namesOrdered = append(f.namesOrdered, k)
+	f.nameValues[k] = v
+}
+
+func (f *Fields) get(k string) reflect.Value {
+	return f.nameValues[k]
+}
+
+func (f *Fields) getNames() []string {
+	return f.namesOrdered
+}
+
 // getId (from Fields) finds id case insensitive inside the fields. Returns the original id key, and its value
 func (f *Fields) getId() (key string, value reflect.Value, err error) {
 	for fieldName, fieldValue := range f.nameValues {
@@ -105,4 +84,25 @@ func sanitizeInput(query string, args ...any) string {
 	}
 
 	return fmt.Sprintf(query, args...)
+}
+
+// getStructFromPointer unreferences the pointer until it gets a struct
+func getStructFromPointer(s any) (val reflect.Value, err error) {
+	val = reflect.ValueOf(s)
+
+	kind := val.Kind()
+	if kind == reflect.Pointer {
+		return getStructFromPointer(val.Elem().Interface())
+	}
+
+	if kind != reflect.Struct {
+		err = ErrNoStruct
+	}
+
+	return
+}
+
+// getStructName returns the name of the struct as a string
+func getStructName(val reflect.Value) string {
+	return reflect.TypeOf(val.Interface()).Name()
 }
